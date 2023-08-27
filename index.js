330cdeabd28815ff0f6dc19e2d89d23c2be74364
ShamBB <sham_berani@yahoo.com>
2023-08-27 20:52:59 +0800

    Paassword generator File

---
+++ index.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordsFirst = document.getElementById("firstPasswordText");
+let passwordsSecond = document.getElementById("secondPasswordText");
+
+function randomIndex() {
+  return Math.floor(Math.random() * characters.length);
+}
+
+function randomPassword() {
+  let password = "";
+  for (let i = 0; i < 30; i++) {
+    password += characters[randomIndex()];
+  }
+  return password;
+}
+
+function generatePassword() {
+  passwordsFirst.textContent = randomPassword();
+  passwordsSecond.textContent = randomPassword();
+}
